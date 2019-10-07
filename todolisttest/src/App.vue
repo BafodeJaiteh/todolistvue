f2cb8372b7786d1db09b168bf3c5579b9e17a7cb
bafode <bafodejaiteh2@gmail.com>
2019-10-07 14:15:05 +0200

    clean boiler plate

---
+++ todolisttest/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">  
+    <h1> To Do List </h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "app",
+  components: {
+  }
+};
+</script>npm install bulma
+
+<style lang="scss">
+
+</style>
---
+++ todolisttest/src/main.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
+" import './../node_modules/bulma/css/bulma.css';
